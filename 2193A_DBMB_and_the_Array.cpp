61243b8ee72f8ba4fd2bf8f3da268af89c19d1ff
Hira-Ranjan <hira60945@gmail.com>
2026-08-01 00:20:25 +0600

    Sync Codeforces: 2193A - DBMB and the Array [C++17 (GCC 7-32)]

---
+++ 2193A_DBMB_and_the_Array.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  ios:: sync_with_stdio(false);
+  cin.tie(nullptr);
+ 
+  int t;
+  cin >> t;
+  while(t--){
+    int n,s,x;
+    cin >> n >> s >> x;
+    vector<int>v(n);
+    for(int i=0;i<n;i++){
+        cin >> v[i];
+    }
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum+=v[i];
+    }
+    if(sum>s){
+        cout << "NO" << endl;
+    }
+    else if(sum==s){
+        cout << "YES" << endl;
+    }
+    else {
+        if((s-sum)%x==0){
+            cout << "YES" << endl;
+        }
+        else {
+            cout << "NO" << endl;
+        }
+    }
+  }
+return 0;
+}
