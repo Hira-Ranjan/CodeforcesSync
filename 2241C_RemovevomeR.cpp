f8b6fc1c636ed42efe04f490818a449365799d69
Hira-Ranjan <hira60945@gmail.com>
2026-08-01 00:20:59 +0600

    Sync Codeforces: 2241C - RemovevomeR [C++17 (GCC 7-32)]

---
+++ 2241C_RemovevomeR.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int t;
+  cin >> t;
+  while(t--){
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int cnt=0;
+    for(int i=1;i<n;i++){
+        if(s[i]!=s[i-1])
+        cnt++;
+    }
+    if(cnt==1){
+        cout << 2 << endl;
+    }
+    else {
+        cout << 1 << endl;
+    }
+  }
+return 0;
+}
