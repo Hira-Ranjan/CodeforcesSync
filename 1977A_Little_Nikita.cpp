9f8dea97f0fdbe44dc6022c165541ca014a47133
Hira-Ranjan <hira60945@gmail.com>
2026-08-16 23:26:06 +0600

    Sync Codeforces: 1977A - Little Nikita [C++17 (GCC 7-32)]

---
+++ 1977A_Little_Nikita.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+ 
+  int t;
+  cin >> t;
+  while(t--){
+    int n,m;
+    cin >> n >> m;
+    if(n>=m && (n-m)%2==0){
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+  }
+return 0;
+}
